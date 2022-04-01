6a6df81fa19908649ed5487e4e6d997156ad451b
jurando1 <46798502+jurando1@users.noreply.github.com>
2022-04-01 18:42:30 +0200

    Idle fix

--- Player.gd
+++ Player.gd
@@ -27,46 +27,55 @@ func activate_animation(state):
 func _physics_process(_delta):
 	
 	motion.y += GRAVITY
-	if motion.y >= MAXFALLSPEED:
+	if motion.y > MAXFALLSPEED:
 		motion.y = MAXFALLSPEED
 	
 		# $Sprite.scale.x = -1
 	
 	motion.x = clamp(motion.x,-MAXSPEED,MAXSPEED)
-	if Input.is_action_pressed("left"):
+	
+	if motion.x == 0 and is_on_floor():
+		$AnimationPlayer.play("Idle")
+	elif motion.x != 0 and is_on_floor():
+		$AnimationPlayer.play("Run")
+		
+	if Input.is_action_pressed("left") and Input.is_action_pressed("right"):
+		motion.x = 0
+#		if is_on_floor():
+#			$AnimationPlayer.play("Idle")
+			
+	elif Input.is_action_pressed("left"):
 		motion.x -= ACCELERATION
 		facing_right = false
 		$Sprite.scale.x = -1
-		if ($AnimationPlayer.current_animation != "Fall") and ($AnimationPlayer.current_animation != "Jump") or is_on_floor():
-			$AnimationPlayer.play("Run")
 	elif Input.is_action_pressed("right"):
 		motion.x += ACCELERATION
 		facing_right = true
 		$Sprite.scale.x = 1
-		if ($AnimationPlayer.current_animation != "Fall") and ($AnimationPlayer.current_animation != "Jump") or is_on_floor():
-			$AnimationPlayer.play("Run")
 		
 	else:
-		if Input.is_action_pressed("attack"):
-			motion.x = lerp(motion.x,0,0.2)
-			$AnimationPlayer.play("attack")
-			$Gun.play()
+		#if Input.is_action_pressed("attack"):
+		#	motion.x = lerp(motion.x,0,0.2)
+		#	$AnimationPlayer.play("attack")
+		#	$Gun.play()
 			
-		elif ($AnimationPlayer.current_animation != "Fall") and ($AnimationPlayer.current_animation != "Jump") or (!Input.is_action_pressed("left")) or (!Input.is_action_pressed("right")) or (!Input.is_action_pressed("attack")) or (!Input.is_action_pressed("jump")):
-			motion.x = lerp(motion.x,0,0.2)
-			if is_on_floor():
-				$AnimationPlayer.play("Idle")
+		if ($AnimationPlayer.current_animation != "Fall") or ($AnimationPlayer.current_animation != "Jump") or (!Input.is_action_pressed("left")) or (!Input.is_action_pressed("right")) or (!Input.is_action_pressed("attack")) or (!Input.is_action_pressed("jump")):
+			#motion.x = lerp(motion.x,0,0.2)
+			motion.x = 0
+#			if is_on_floor():
+#				$AnimationPlayer.play("Idle")
+
 		
 
 	if is_on_floor():
-		if Input.is_action_pressed("jump"):
+		if Input.is_action_just_pressed("jump"):
 			$Jump.play()
 			motion.y = -JUMPFORCE
 	
 	if !is_on_floor():
-		if motion.y <= 0:
+		if motion.y < 0:
 			$AnimationPlayer.play("Jump")
-		elif motion.y >= 0:
+		elif motion.y > 0:
 			$AnimationPlayer.play("Fall")
 
 	motion = move_and_slide(motion,UP)
